b2547476089a953814a3866f0a6d37bab6a7a758
Walter Doekes <walter+github@wjd.nu>
2009-08-06 12:54:59 +0000

    Fix so DONT_STORE_ZERO_ENTRIES works with limited mysql accounts (that are not allowed to DELETE rows).

--- lightcount/daemon/storage_my.c
+++ lightcount/daemon/storage_my.c
@@ -30,7 +30,7 @@ along with LightCount.  If not, see <http://www.gnu.org/licenses/>.
 static char const *storage__config_file;    /* configuration file name */
 static MYSQL *storage__mysql;		    /* gets reinitialized every write */
 static int storage__node_id;		    /* may vary per write */
-static uint32_t storage__unixtime_begin;   /* varies per write */
+static uint32_t storage__unixtime_begin;    /* varies per write */
 static uint32_t storage__interval;	    /* may vary per write */
 
 static char storage__conf_host[256];	    /* db hostname/ip */
@@ -70,7 +70,7 @@ void storage_help() {
 	"\n"
 	"When DONT_STORE_ZERO_ENTRIES is defined, no values with all zeroes are stored.\n"
 	"For calculation purposes no value is the same as all zeroes anyway. But if\n"
-	"you're interested to see whether there has been _any_ traffic at all, you'll\n"
+	"you're interested in seeing whether there has been _any_ traffic at all, you'll\n"
 	"want to undefine it.\n"
 	"\n",
 #ifdef DONT_STORE_ZERO_ENTRIES
@@ -120,19 +120,6 @@ void storage_write(uint32_t unixtime_begin, uint32_t interval, void *memory) {
     
     /* Finally! Insert data! */
     memory_enum(memory, &storage__write_ip);
-
-#ifdef DONT_STORE_ZERO_ENTRIES
-    /* Drop all entries that have all values 0. All values zero is the default anyway (no traffic). */
-    sprintf(
-	buf,
-	"DELETE FROM sample_tbl "
-	"WHERE unixtime = %" SCNu32 " AND node_id = %i"
-	" AND in_pps = 0 AND in_bps = 0 AND out_pps = 0 AND out_bps = 0",
-	storage__unixtime_begin, storage__node_id
-    );
-    if (mysql_query(storage__mysql, buf))
-        fprintf(stderr, "mysql_query: %s\n", mysql_error(storage__mysql));
-#endif /* DONT_STORE_ZERO_ENTRIES */
 
     /* Disconnect */
     storage__db_disconnect();
@@ -267,12 +254,20 @@ static void storage__write_ip(uint32_t ip, struct ipcount_t const *ipcount) {
 	    "SELECT ip_begin FROM ip_range_tbl "
 	    "WHERE ip_begin <= %" SCNu32 " AND %" SCNu32 " <= ip_end"
 	    " AND (node_id IS NULL OR node_id = %i)"
+#ifdef DONT_STORE_ZERO_ENTRIES
+	    " AND (ROUND(%" SCNu32 "/%" SCNu32 ")<>0 OR ROUND(%" SCNu64 "/%" SCNu32 ")<>0"
+	    " OR ROUND(%" SCNu32 "/%" SCNu32 ")<>0 OR ROUND(%" SCNu64 "/%" SCNu32 ")<>0)"
+#endif /* DONT_STORE_ZERO_ENTRIES */
 	")",
 	storage__unixtime_begin, storage__node_id, ipcount->vlan, ip,
 	ipcount->packets_in, storage__interval, ipcount->u.bytes_in, storage__interval,
 	ipcount->packets_out, storage__interval, ipcount->bytes_out, storage__interval,
 	ip, ip, storage__node_id
-    ); /* 15 args * len("18446744073709551615") is still only 300 */
+#ifdef DONT_STORE_ZERO_ENTRIES
+	, ipcount->packets_in, storage__interval, ipcount->u.bytes_in, storage__interval,
+	ipcount->packets_out, storage__interval, ipcount->bytes_out, storage__interval
+#endif /* DONT_STORE_ZERO_ENTRIES */
+    ); /* 23 args * len("18446744073709551615") is still only 460 */
     if (mysql_query(storage__mysql, buf)) {
 	fprintf(stderr, "mysql_query: %s\n", mysql_error(storage__mysql));
 	return;
